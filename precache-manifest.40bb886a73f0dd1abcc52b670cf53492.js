e39a2eaf2d83f1e3e988671398e2278eecc32f7f
lauraceban <lauracristina.ceban@gmail.com>
2020-01-03 19:17:01 +0000

    Updates

---
+++ precache-manifest.40bb886a73f0dd1abcc52b670cf53492.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838a456b6b6ccc3bb24874843c636ca4",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "6a38e457560d34740d00",
+    "url": "/todo-react-app/static/css/main.142f8a55.chunk.css"
+  },
+  {
+    "revision": "e9f99ee6506b145bc3ab",
+    "url": "/todo-react-app/static/js/2.afa5c41a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-react-app/static/js/2.afa5c41a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a38e457560d34740d00",
+    "url": "/todo-react-app/static/js/main.cb560ebd.chunk.js"
+  },
+  {
+    "revision": "e3c4f471c0b6661b2689",
+    "url": "/todo-react-app/static/js/runtime-main.36e692a3.js"
+  },
+  {
+    "revision": "cd4b80bdcd156a9ac36d14f4294b267b",
+    "url": "/todo-react-app/static/media/Blue.cd4b80bd.jpg"
+  },
+  {
+    "revision": "42773c5814879b5fc12b22a82735066e",
+    "url": "/todo-react-app/static/media/bg-img.42773c58.jpg"
+  },
+  {
+    "revision": "6e128a0fba86660798b9194ba6250d68",
+    "url": "/todo-react-app/static/media/waltz.6e128a0f.mp3"
+  }
+]);
